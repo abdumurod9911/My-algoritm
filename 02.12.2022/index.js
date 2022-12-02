4e03ed2c14617628a89aa63c431b7048437c2903
abdumurod9911 <khasanboyevabdumurod2@gmail.com>
2022-12-02 20:34:49 +0300

    fix

--- 02.12.2022/index.js
+++ 02.12.2022/index.js
@@ -4,10 +4,42 @@
 //  myName( "Jack")
 
 
+
+
 // function getMyinfo(name, age, job) {
 //     return `my name is ${name}, my age ${age}, my job ${job},`;
 // }
 // console.log(getMyinfo("Jack","23", "write code"));
+
+
+
+
+// function getInfo(a, b, c) {
+//     let data = {
+//         a: a,
+//         b: b,
+//         c: c
+//     }
+   
+//     return `My name is ${data.a}. my age ${data.b} my car ${data.c}.`
+// }
+// alert(getInfo("Jack", 23, "Scoda"));
+
+
+
+// function addition(numm){
+//   return numm+1
+// }
+// console.log(addition(9));
+
+
+
+
+
+// function seconds(minut){
+//   return minut*60
+// }
+// console.log(secund(3)):
 
 
 
