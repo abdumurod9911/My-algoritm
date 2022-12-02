5db11b78abefa2f6361f0d19ebf22383c4ddf4d2
abdumurod9911 <khasanboyevabdumurod2@gmail.com>
2022-12-02 20:10:26 +0300

    fix

---
+++ 02.12.2022/index.js
@@ -0,0 +1,13 @@
+// function myName(name){
+//     console.log("my name is " +name);
+// }
+//  myName( "Jack")
+
+
+// function getMyinfo(name, age, job) {
+//     return `my name is ${name}, my age ${age}, my job ${job},`;
+// }
+// console.log(getMyinfo("Jack","23", "write code"));
+
+
+
